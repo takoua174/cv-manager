18aa83e0af9bf0fcc19de7754c9aa5a742a6598e
takoua174 <taki74ayadi@gmail.com>
2025-04-17 17:59:34 +0100

    injecting generic service in other services

--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCvDto } from './dto/create-cv.dto';
-import { UpdateCvDto } from './dto/update-cv.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { GenericService } from 'src/common/services/generic.service';
+import { Cv } from 'src/cv/entities/cv.entity';
+import { Repository } from 'typeorm';
+//hadha ymarki el class as provider
 @Injectable()
-export class CvService {
-  create(createCvDto: CreateCvDto) {
-    return 'This action adds a new cv';
-  }
-
-  findAll() {
-    return `This action returns all cv`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} cv`;
-  }
-
-  update(id: number, updateCvDto: UpdateCvDto) {
-    return `This action updates a #${id} cv`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} cv`;
+export class CvService extends GenericService<Cv> {
+  constructor(
+    @InjectRepository(Cv)
+    private readonly cvRepository: Repository<Cv>,
+  ) {
+    super(cvRepository);
   }
 }
--- src/skill/skill.service.ts
+++ src/skill/skill.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSkillDto } from './dto/create-skill.dto';
 import { UpdateSkillDto } from './dto/update-skill.dto';
+import { GenericService } from 'src/common/services/generic.service';
+import { Skill } from './entities/skill.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SkillService {
-  create(createSkillDto: CreateSkillDto) {
-    return 'This action adds a new skill';
-  }
-
-  findAll() {
-    return `This action returns all skill`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} skill`;
-  }
-
-  update(id: number, updateSkillDto: UpdateSkillDto) {
-    return `This action updates a #${id} skill`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} skill`;
+export class SkillService extends GenericService<Skill> {
+  constructor(
+    @InjectRepository(Skill)
+    private readonly skillRepository: Repository<Skill>,
+  ) {
+    super(skillRepository);
   }
 }
